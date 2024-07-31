354cb510f41b05932e1a48c4166c10a6221f6a9c
anotherITdude <abhi.baidya@gmail.com>
2024-07-31 13:38:57 +0200

    New: hero section v1

--- app/ar/page.tsx
+++ app/ar/page.tsx
@@ -1,10 +1,10 @@
 import Container from "@/components/Container";
-import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <Container>
-      <Navbar />
+      <Hero />
     </Container>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Container from "@/components/Container";
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <Container>
-      <Navbar />
+      <Hero />
     </Container>
   );
 }
