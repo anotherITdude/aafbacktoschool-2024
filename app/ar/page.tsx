56c6616fad5ed64212e9a030c217a732a733dc5a
anotherITdude <abhi.baidya@gmail.com>
2024-08-02 17:21:50 +0200

    New: winlist final section

--- app/ar/page.tsx
+++ app/ar/page.tsx
@@ -1,12 +1,14 @@
 import Competition from "@/components/Competition";
 import Container from "@/components/Container";
 import Hero from "@/components/Hero";
+import Winlist from "@/components/Winlist";
 
 export default function Home() {
   return (
     <Container>
       <Hero />
       <Competition />
+      <Winlist />
     </Container>
   );
 }
